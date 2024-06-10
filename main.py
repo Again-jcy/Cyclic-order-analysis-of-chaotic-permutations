f489eea374e7a1d5308e81e19c407619738f633a
Again-jcy <chenying.jiang@outlook.com>
2024-06-10 12:58:29 +0800

    main.py

--- main.py
+++ main.py
@@ -32,25 +32,27 @@ def main():
     
     # 生成置乱表并进行测评
     for x0, y0 in seeds:
-        permutation = evaluator.generator.generate_permutation(x0, y0, M, N)  
+        permutation = evaluator.generator.generate_permutation(x0, y0, M, N)
+        print("permutation:") 
+        print(permutation)
         permutation_list.append(permutation)
         # 统计置乱表的循环情况
         cycle_length_counts, total_order = evaluator.find_cycles(permutation)
         total_order_list.append(total_order)
-        print(f"循环圈的长度和个数: {dict(cycle_length_counts)}")
-        print(f"置乱表的总阶: {total_order}")
+        print(f"Cycle lengths and counts: {dict(cycle_length_counts)}")
+        print(f"Total order: {total_order}")
         # 敏感性分析
         sensitivity = evaluator.sensitivity_tests(permutation, x0, y0, M, N)
-        print(f"修改种子前后置乱表的相似程度: {sensitivity:.5f}")
+        print(f"Similarity between original and modified: {sensitivity:.5f}")
         sensitivity_list.append(sensitivity)
  
     average_order = np.mean(total_order_list)
-    print(f"平均的阶:{average_order}")
+    print(f"Average order:{average_order}")
     average_sensitivity = np.mean(sensitivity_list) #
-    print(f"修改种子前后置乱表的平均相似程度 : {average_sensitivity:.5f}")
+    print(f"(Sensitivity)Average similarity between original and modified : {average_sensitivity:.5f}")
     p_values, combined_p_value = evaluator.randomness_tests(permutation_list)
     #print(p_values)
-    print(f"p值:{combined_p_value}")
+    print(f"p_value:{combined_p_value}")
    
     # 绘制平均阶-N曲线
     #average_orders = evaluator.plot_average_order_vs_N(M, N_values, seeds)
